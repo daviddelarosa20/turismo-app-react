e281bcc4904032ef4e9e83a42cd8b55503d97148
Luz-ingsof <luzdelcarmenortiz.ramirez@gmail.com>
2025-05-12 20:23:15 -0600

    boton d ayuda

--- app/(tabs)/Settings.jsx
+++ app/(tabs)/Settings.jsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
 
 export default function Settings() {
   return (
-    <View>
-      <Text>Esta es el tab de settings</Text>
-    </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      
+
+      {/* BOTÓN DE SERVICIO DE AYUDA */}
+      <Link href="/help/helpcenter" asChild>
+        <TouchableOpacity className="bg-white-500 px-4 py-2 rounded">
+          <Text className="text-black text-lg">Servicio de ayuda</Text>
+        </TouchableOpacity>
+      </Link>
+    </View> 
   );
 }

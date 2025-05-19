3f570d253bc5a6d2d4f9b5a7b0cd5a08be949922
Miguel Ramos <mramos@sc-computacion.com>
2025-05-18 20:19:52 -0600

    PlanBoletaje

--- app/planes/PlanBoletaje.jsx
+++ app/planes/PlanBoletaje.jsx
@@ -37,8 +37,6 @@ export default function PlanBoletaje() {
     });
   }, [navigation]);
 
-  // const { title, description, direccion, imageUrl } = useLocalSearchParams();
-
   const [valoracion, setValoracion] = useState(null);
   const [empresa, setEmpresa] = useState(null);
 

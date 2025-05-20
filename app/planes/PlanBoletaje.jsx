b8fc2d368386948d990c30b29acedae12deb7ac3
Miguel Ramos <mramos@sc-computacion.com>
2025-05-19 22:04:48 -0600

    Cambios en el diseño de las pantllas

--- app/planes/PlanBoletaje.jsx
+++ app/planes/PlanBoletaje.jsx
@@ -25,17 +25,18 @@ export default function PlanBoletaje() {
   useLayoutEffect(() => {
     navigation.setOptions({
       title: title || "Detalle",
-      headerStyle: { backgroundColor: "#e1dcd0" },
+      headerStyle: { backgroundColor: "#282d33" },
       headerTitleAlign: "center",
+      headerTintColor: "#F5EFE7",
       headerRight: () => (
-        <View className="items-center justify-center mr-3 rounded-full bg-slate-100 p-2">
+        <View className="items-center justify-center mr-3 rounded-full bg-slate-800 p-2">
           <TouchableOpacity onPress={() => alert("Perfil")}>
-            <AntDesign name="user" size={24} color="black" />
+            <AntDesign name="user" size={24} color="#F5EFE7" />
           </TouchableOpacity>
         </View>
       ),
     });
-  }, [navigation]);
+  }, [navigation, title]);
 
   const [valoracion, setValoracion] = useState(null);
   const [empresa, setEmpresa] = useState(null);
@@ -54,7 +55,8 @@ export default function PlanBoletaje() {
   };
   useEffect(() => {
     getempresa();
-  }, []);
+  }, [title]);
+
   const getvaloracion = async () => {
     let { data: Valoracion, error } = await supabase
       .from("Empresas")
@@ -69,7 +71,7 @@ export default function PlanBoletaje() {
   };
   useEffect(() => {
     getvaloracion();
-  }, []);
+  }, [title]);
 
   const staticImages = [
     "https://jxcchonixqmpsnyefhfh.supabase.co/storage/v1/object/public/images//Teatro.jpg",
@@ -79,94 +81,105 @@ export default function PlanBoletaje() {
     "https://jxcchonixqmpsnyefhfh.supabase.co/storage/v1/object/public/images//Image.jpg",
   ];
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <ScrollView>
-        <View className="w-full h-full mb-12">
-          <View className="flex-1 items-center justify-center mt-10 mb-14">
-            <Text className="text-3xl font-bold mb-2">{title}</Text>
-            <View className="flex-row items-center justify-center mb-5">
+    <SafeAreaView className="flex-1 bg-darkBlue-900">
+      {/* Fondo oscuro */}
+      <ScrollView className="flex-1">
+        <View className="p-4 pt-2 items-center">
+          <View className="items-center mb-8">
+            <Text className="text-3xl font-bold text-veryLightBeige-500 mb-2">
+              {title}
+            </Text>{" "}
+            {/* Texto claro */}
+            <View className="flex-row items-center mb-4">
               {valoracion?.Valoracion && (
-                <StarRating rating={valoracion.Valoracion} />
+                <StarRating
+                  rating={valoracion.Valoracion}
+                  starSize={20}
+                  starColor="#D8C4B6"
+                />
               )}
-              <Text className="text-xl font-bold ml-2 color-slate-500">
+              <Text className="text-xl font-bold ml-2 text-lightBeige-400">
+                {" "}
+                {/* Texto beige */}
                 {valoracion?.Valoracion}
               </Text>
             </View>
-            <ScrollView
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              className="w-full mb-1"
-            >
-              {staticImages.map((image, index) => (
-                <Image
-                  key={index}
-                  source={{ uri: image }}
-                  className="w-80 h-72 rounded-xl mx-2 bg-gray-400"
-                />
-              ))}
-            </ScrollView>
-            <Text className="text-xl text-start font-bold">
+          </View>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            className="w-full mb-6"
+            contentContainerStyle={{ paddingRight: 16 }}
+          >
+            {staticImages.map((image, index) => (
+              <Image
+                key={index}
+                source={{ uri: image }}
+                className="w-80 h-52 rounded-xl mr-4 bg-gray-700"
+              />
+            ))}
+          </ScrollView>
+          <View className="w-full items-start mb-6">
+            <Text className="text-xl font-bold text-veryLightBeige-500 mb-2">
               Información del lugar
             </Text>
-            <Text className="text-lg font-normal text-start">
+            <Text className="text-lg font-normal text-lightBeige-400">
               {description}
             </Text>
-            <View className="flex-row justify-around mt-6 gap-12">
-              {[
-                "https://cdn-icons-png.flaticon.com/512/1384/1384063.png",
-                "https://img.icons8.com/ios_filled/512/twitterx.png",
-                "https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png",
-                "https://static.vecteezy.com/system/resources/previews/016/716/450/non_2x/tiktok-icon-free-png.png",
-              ].map((uri, idx) => (
-                <Image
-                  key={idx}
-                  source={{ uri }}
-                  style={{ width: 32, height: 32, borderRadius: 4 }}
-                />
-              ))}
-            </View>
-            <Text className="text-lg font-bold mt-7">Ubicación</Text>
-            <Text className="text-lg font-normal mb-1 mr-30 mt-2">
+          </View>
+          <View className="flex-row justify-around mt-4 mb-8 w-4/5">
+            {[
+              "https://cdn-icons-png.flaticon.com/512/1384/1384063.png",
+              "https://img.icons8.com/ios_filled/512/twitterx.png",
+              "https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png",
+              "https://static.vecteezy.com/system/resources/previews/016/716/450/non_2x/tiktok-icon-free-png.png",
+            ].map((uri, idx) => (
+              <Image
+                key={idx}
+                source={{ uri }}
+                className="w-8 h-8 rounded-md"
+              />
+            ))}
+          </View>
+          <View className="w-full items-start mb-8">
+            <Text className="text-lg font-bold text-veryLightBeige-500 mb-2">
+              Ubicación
+            </Text>
+            <Text className="text-lg font-normal text-lightBeige-400 mb-3">
               {direccion}
             </Text>
-            <View className="w-full h-60 mt-0 mb-1 px-4">
-              <TouchableOpacity
-                onPress={() => {
-                  const query = encodeURIComponent(
-                    `${empresa?.Calle} ${empresa?.NumExt}, ${empresa?.Colonia}, ${empresa?.Ciudad}`,
-                  );
-                  Linking.openURL(
-                    `https://www.google.com/maps/search/?api=1&query=${query}`,
-                  );
+            <TouchableOpacity
+              onPress={() => {
+                const query = encodeURIComponent(
+                  `${empresa?.Calle} ${empresa?.NumExt}, ${empresa?.Colonia}, ${empresa?.Ciudad}`,
+                );
+                Linking.openURL(`http://google.com/maps/place/${query}`);
+              }}
+              className="w-full h-48 rounded-xl overflow-hidden"
+            >
+              <ImageBackground
+                source={{
+                  uri: "https://www.tintasytonercompatibles.es/images/blog/como-imprimir-mapa-google-maps.jpg",
                 }}
-                className="w-full h-60 mt-0 mb-5"
-              >
-                <ImageBackground
-                  source={{
-                    uri: "https://www.tintasytonercompatibles.es/images/blog/como-imprimir-mapa-google-maps.jpg",
-                  }}
-                  className="w-full h-60 mt-10 mb-5 border rounded-xl"
-                ></ImageBackground>
-              </TouchableOpacity>
-            </View>
-            <View>
-              <TouchableOpacity
-                className="mt-20 bg-green-200 px-8 py-3 rounded-full"
-                onPress={() => {
-                  router.push({
-                    pathname: "/planes/Evento",
-                    params: {
-                      title: title,
-                    },
-                  });
-                }}
-              >
-                <Text className="font-semibold text-black">
-                  Ver próximos eventos
-                </Text>
-              </TouchableOpacity>
-            </View>
+                className="w-full h-full"
+              />
+            </TouchableOpacity>
           </View>
+          <TouchableOpacity
+            className="mt-10 bg-green-300 px-8 py-3 rounded-full"
+            onPress={() => {
+              router.push({
+                pathname: "/planes/Evento",
+                params: {
+                  title: title,
+                },
+              });
+            }}
+          >
+            <Text className="font-semibold text-darkBlue-900">
+              Ver próximos eventos
+            </Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
